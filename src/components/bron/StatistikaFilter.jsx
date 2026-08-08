05ef6943dcb6aa8f41cd652416d46d41114505f8
Kaisa Liiv <kaisa.liiv@Mac.lan>
2026-08-08 22:00:50 +0300

    feat: filtris ruumi valik (hoone + ruum + ruumitüüp + periood)

--- src/components/bron/StatistikaFilter.jsx
+++ src/components/bron/StatistikaFilter.jsx
@@ -1,17 +1,33 @@
-import { HOONED, RUUMITYYBID } from '../../BronStatisticsService';
+import { HOONED, RUUMID, RUUMITYYBID } from '../../BronStatisticsService';
 
 export default function StatistikaFilter({ filters, onChange }) {
     function set(key, value) {
         onChange({ ...filters, [key]: value });
     }
 
+    const ruumidNimekirjas = filters.hoone?.[0]
+        ? RUUMID.filter(r => r.hoone === filters.hoone[0])
+        : RUUMID;
+
+    function handleHooneChange(e) {
+        onChange({ ...filters, hoone: e.target.value ? [e.target.value] : [], ruum_id: '' });
+    }
+
     return (
         <div className="bron-filters">
             <div className="bron-form-group" style={{ minWidth: 160 }}>
                 <label>Hoone</label>
-                <select value={filters.hoone?.[0] || ''} onChange={e => set('hoone', e.target.value ? [e.target.value] : [])}>
+                <select value={filters.hoone?.[0] || ''} onChange={handleHooneChange}>
                     <option value="">Kõik hooned</option>
                     {HOONED.map(h => <option key={h.code} value={h.code}>{h.code}</option>)}
+                </select>
+            </div>
+
+            <div className="bron-form-group" style={{ minWidth: 160 }}>
+                <label>Ruum</label>
+                <select value={filters.ruum_id || ''} onChange={e => set('ruum_id', e.target.value)}>
+                    <option value="">Kõik ruumid</option>
+                    {ruumidNimekirjas.map(r => <option key={r.id} value={r.id}>{r.name}</option>)}
                 </select>
             </div>
 
@@ -35,7 +51,7 @@ export default function StatistikaFilter({ filters, onChange }) {
 
             <button
                 className="bron-btn bron-btn-secondary"
-                onClick={() => onChange({ hoone: [], ruumitypp: [] })}
+                onClick={() => onChange({ hoone: [], ruumitypp: [], ruum_id: '' })}
                 style={{ alignSelf: 'flex-end' }}
             >
                 Tühjenda
